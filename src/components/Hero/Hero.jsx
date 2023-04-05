d6fa8be3455c325dca2f7389acd82f09d4438f24
shhady <shhadyse@gmail.com>
2023-04-05 20:31:05 +0300

    try

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
      תתחיל את <br/>  המסע שלך 
      <span className="TODAY"> היום</span>
     </h1>
-    <p> תתחיל להתקרב לגוף שחלמת עליו</p>
+    {/* <p> תתחיל להתקרב לגוף שחלמת עליו</p> */}
     <div>
      <button className="joinNow">צור קשר</button>
     </div>
